2f4bed9f6877944ab23940b30f9096024faa3a6d
Mikael Karlsson <maajkan@live.se>
2025-10-21 12:49:06 +0200

    Feature: Lade till alla klasser för grunden

---
+++ E-commerce-console/Products.cs
@@ -0,0 +1,6 @@
+﻿namespace E_commerce_console;
+
+public class Products
+{
+    
+}
--- E-commerce-console/Program.cs
+++ E-commerce-console/Program.cs
@@ -1,4 +1,12 @@
 ﻿namespace E_commerce_console;
+/*
+Uppgiften 
+Du ska skapa en konsolapplikation i C# där användaren välja produkter att lägga till i sin varukorg.  
+Efter att ha lagt in minst en produkt i sin varukorg kan användaren välja att gå till kassan varefter 
+användaren kan se totalbelopp och bekräfta sin order. Då skapas det en order. 
+Bonus – Lägg in momssats på produkter, och låt användaren se “Varav moms” i kassan – Lägg in inköpspris på produkter, så att t.ex. en admin kan se dagens ordrar och hur mycket vinst 
+som hittills genererats.
+*/
 
 class Program
 {
